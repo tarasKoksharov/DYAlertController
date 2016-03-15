d719812f0c440d339c0097775754094da3318be3
Dominik <dominikbutz@gmail.com>
2016-03-15 16:18:55 +0800

    Initial Commit

---
+++ DYAlertController/DYAlertController.h
@@ -0,0 +1,19 @@
+//
+//  DYAlertController.h
+//  DYAlertController
+//
+//  Created by Dominik Butz on 15/03/16.
+//  Copyright © 2016 Duoyun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DYAlertController.
+FOUNDATION_EXPORT double DYAlertControllerVersionNumber;
+
+//! Project version string for DYAlertController.
+FOUNDATION_EXPORT const unsigned char DYAlertControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DYAlertController/PublicHeader.h>
+
+
